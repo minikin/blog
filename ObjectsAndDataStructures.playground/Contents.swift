0e9efb8a45362ab8a1a2c38f6bd7c1701004f7c9
minikin <djminikin@gmail.com>
2017-09-28 22:30:03 +0200

    Add java style object example

--- ObjectsAndDataStructures.playground/Contents.swift
+++ ObjectsAndDataStructures.playground/Contents.swift
@@ -2,3 +2,61 @@
 
 import UIKit
 
+// Object - Procedural Shape
+
+typealias Point = (Double, Double)
+
+struct Square {
+  let topLeft: Point
+  let side: Double
+}
+
+struct Rectangle {
+  let topLeft: Point
+  let height: Double
+  let width: Double
+}
+
+struct Circle {
+  let center: Point
+  let radius: Double
+}
+
+struct Geometry {
+  func area(_ shape: Any) -> Double {
+      if shape is Square {
+        print("Shape is Square")
+        let s = shape as! Square
+        return s.side * s.side
+      } else if shape is Rectangle {
+        print("Shape is Rectangle")
+        let r = shape as! Rectangle
+        return r.height * r.width
+      } else if shape is Circle {
+        print("Shape is Circle")
+        let c = shape as! Circle
+        return c.radius * c.radius * .pi
+      } else {
+        print("Unknow object")
+        return 0
+    }
+  }
+}
+
+let square = Square(topLeft: (0,0), side: 10)
+let rectangle = Rectangle(topLeft: (0,0), height: 20, width: 15)
+let circle = Circle(center: (0,0), radius: 25)
+let unknowObject = "Hey!"
+
+let geometry = Geometry()
+
+geometry.area(square)
+geometry.area(rectangle)
+geometry.area(circle)
+geometry.area(unknowObject)
+
+
+
+
+
+// Data structure
--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Thoughts how to define/use Objects and Data Structures
 
 Please read full blog post [here]() or [here]()
 
+## Requirements
+
+- iOS 8.0+ / macOS 10.10+ / tvOS 9.0+ / watchOS 2.0+
+- Xcode 9.0+
+- Swift 4.0+
+
 Let me know  what do you think @minikin
